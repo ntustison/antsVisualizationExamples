f2398b6e4474fe01d8fff58495cf1c58a50b30a6
Nick Tustison <ntustison@gmail.com>
2015-05-12 10:36:08 -0700

    ENH:  Finalizing commands

--- antsSurfCommand.sh
+++ antsSurfCommand.sh
@@ -1,39 +1,58 @@
 #!/bin/bash
 
-# Borrowed from https://github.com/stnava/antsSurf/blob/master/GenerateBrainMesh3.sh
-# Showing:
-#   1. Thickness
-#   2.
-
 INPUT_DIRECTORY=${PWD}/Images/
 OUTPUT_DIRECTORY=${PWD}/OutputAntsSurf/
+
+mkdir -p $OUTPUT_DIRECTORY
 
 INPUT_T1=${INPUT_DIRECTORY}/OAS1_0061_MR1_mpr_n4_anon_sbj_111.nii.gz
 INPUT_SEGMENTATION=${INPUT_DIRECTORY}/OAS1_0061_MR1_mpr_n4_anon_sbj_111BrainSegmentation.nii.gz
 INPUT_THICKNESS=${INPUT_DIRECTORY}/OAS1_0061_MR1_mpr_n4_anon_sbj_111CorticalThickness.nii.gz
+INPUT_JLF=${INPUT_DIRECTORY}/OAS1_0061_MR1_mpr_n4_anon_sbj_111-refinedMalf.nii.gz
 
 OUTPUT_GM=${OUTPUT_DIRECTORY}/grayMatterMask.nii.gz
 OUTPUT_GM_SMOOTH=${OUTPUT_DIRECTORY}/grayMatterSmoothMask.nii.gz
 OUTPUT_THICKNESS_RGB=${OUTPUT_DIRECTORY}/thicknessRgb.nii.gz
 OUTPUT_LOOKUP_TABLE=${OUTPUT_DIRECTORY}/lookupTable.csv
+OUTPUT_REGION=${OUTPUT_DIRECTORY}/regionMask.nii.gz
 
+echo "Thresholding out gray matter."
 ${ANTSPATH}/ThresholdImage 3 $INPUT_SEGMENTATION $OUTPUT_GM 2 4 1 0
+echo "Filling the holes."
 ${ANTSPATH}/ImageMath 3 $OUTPUT_GM FillHoles $OUTPUT_GM
+echo "Getting the largest component."
 ${ANTSPATH}/ImageMath 3 $OUTPUT_GM GetLargestComponent $OUTPUT_GM
-${ANTSPATH}/SmoothImage 3 $OUTPUT_GM_SMOOTH 1.0 $OUTPUT_GM_SMOOTH
-${ANTSPATH}/ThresholdImage 3 $OUTPUT_GM_SMOOTH $OUTPUT_GM_SMOOTH 0.1 1000 1 0
+echo "Smoothing the gray matter mask."
+${ANTSPATH}/SmoothImage 3 $OUTPUT_GM 1.0 $OUTPUT_GM_SMOOTH
+echo "Thresholding the smoothed gray matter mask."
+${ANTSPATH}/ThresholdImage 3 $OUTPUT_GM_SMOOTH $OUTPUT_GM_SMOOTH 0.1 10 1 0
 
-${ANTSPATH}/ImageMath 3 $OUTPUT_THICKNESS_RGB GD $INPUT_THICKNESS 2
-${ANTSPATH}/ConvertScalarImageToRGB 3 $OUTPUT_THICKNESS_RGB $OUTPUT_THICKNESS_RGB hot none 0 8 0 255 ${OUTPUT_LOOKUP_TABLE}
+echo "Dilating the thickness image and converting to RGB."
+${ANTSPATH}/ImageMath 3 $OUTPUT_THICKNESS_RGB GD $INPUT_THICKNESS 3
+${ANTSPATH}/ConvertScalarImageToRGB 3 $OUTPUT_THICKNESS_RGB $OUTPUT_THICKNESS_RGB none hot none 0 8 0 255 ${OUTPUT_LOOKUP_TABLE}
+
+echo "Show thickness over the entire brain"
+${ANTSPATH}/antsSurf -d 3 \
+                     -s [${OUTPUT_GM},255x255x255] \
+                     -f [${OUTPUT_THICKNESS_RGB},${OUTPUT_GM_SMOOTH},0.75] \
+                     -i 25 \
+                     -a 0.03 \
+                     -d [0x0x0,192x255x62] \
+                     -b $OUTPUT_LOOKUP_TABLE
+
+
+echo "Show thickness over the right caudal middle frontal (label 2003)"
+${ANTSPATH}/ThresholdImage 3 $INPUT_JLF $OUTPUT_REGION 2003 2003 1 0
+${ANTSPATH}/SmoothImage 3 $OUTPUT_REGION 1.0 $OUTPUT_REGION
+${ANTSPATH}/ThresholdImage 3 $OUTPUT_REGION $OUTPUT_REGION 0.1 10 1 0
 
 ${ANTSPATH}/antsSurf -d 3 \
-                     -s [${OUTPUT_GM},192x255x62] \
-                     -f [${OUTPUT_THICKNESS_RGB},${OUTPUT_GM_SMOOTH},0.5] \
+                     -s [${OUTPUT_GM},255x255x255] \
+                     -f [${OUTPUT_THICKNESS_RGB},${OUTPUT_REGION},0.75] \
                      -i 25 \
-                     -a 0.05 \
-                     -d 1
-
-
+                     -a 0.03 \
+                     -d [0x0x0,192x255x62] \
+                     -b $OUTPUT_LOOKUP_TABLE
 
 
 
--- createTiledMosaicCommand.sh
+++ createTiledMosaicCommand.sh
@@ -1,2 +1,57 @@
 #!/bin/bash
 
+INPUT_DIRECTORY=${PWD}/Images/
+OUTPUT_DIRECTORY=${PWD}/OutputCreateTiledMosaic/
+
+mkdir -p $OUTPUT_DIRECTORY
+
+INPUT_T1=${INPUT_DIRECTORY}/OAS1_0061_MR1_mpr_n4_anon_sbj_111.nii.gz
+INPUT_SEGMENTATION=${INPUT_DIRECTORY}/OAS1_0061_MR1_mpr_n4_anon_sbj_111BrainSegmentation.nii.gz
+INPUT_THICKNESS=${INPUT_DIRECTORY}/OAS1_0061_MR1_mpr_n4_anon_sbj_111CorticalThickness.nii.gz
+INPUT_JLF=${INPUT_DIRECTORY}/OAS1_0061_MR1_mpr_n4_anon_sbj_111-refinedMalf.nii.gz
+
+OUTPUT_THICKNESS_MOSAIC=${OUTPUT_DIRECTORY}/thicknessMosaic.png
+OUTPUT_THICKNESS_RGB=${OUTPUT_DIRECTORY}/thicknessRgb.nii.gz
+OUTPUT_THICKNESS_MASK=${OUTPUT_DIRECTORY}/thicknessMask.nii.gz
+
+OUTPUT_SEGMENTATION_MOSAIC=${OUTPUT_DIRECTORY}/segmentationMosaic.png
+OUTPUT_SEGMENTATION_RGB=${OUTPUT_DIRECTORY}/segmentationRgb.nii.gz
+OUTPUT_SEGMENTATION_MASK=${OUTPUT_DIRECTORY}/segmentationMask.nii.gz
+
+ITKSNAP_COLORMAP=${OUTPUT_DIRECTORY}/snapColormap.txt
+
+echo "Making thickness mask."
+${ANTSPATH}/ThresholdImage 3 ${INPUT_THICKNESS} ${OUTPUT_THICKNESS_MASK} 0 0 0 1
+echo "Converting thickness to RGB."
+${ANTSPATH}/ConvertScalarImageToRGB 3 $INPUT_THICKNESS $OUTPUT_THICKNESS_RGB none hot none 0 8 0 255
+
+echo "Making thickness mosaic."
+${ANTSPATH}/CreateTiledMosaic -i ${INPUT_T1} \
+                              -r ${OUTPUT_THICKNESS_RGB} \
+                              -o ${OUTPUT_THICKNESS_MOSAIC} \
+                              -a 1.0 \
+                              -t -1x-1 \
+                              -d 2 \
+                              -p mask \
+                              -s [3,mask,mask] \
+                              -x ${OUTPUT_THICKNESS_MASK}
+
+echo "0 1 0 0 1 0 1" > $ITKSNAP_COLORMAP
+echo "0 0 1 0 1 1 0" >> $ITKSNAP_COLORMAP
+echo "0 0 0 1 0 1 1" >> $ITKSNAP_COLORMAP
+
+echo "Making segmentation mask."
+${ANTSPATH}/ThresholdImage 3 ${INPUT_SEGMENTATION} ${OUTPUT_SEGMENTATION_MASK} 0 0 0 1
+echo "Converting thickness to RGB."
+${ANTSPATH}/ConvertScalarImageToRGB 3 ${INPUT_SEGMENTATION} ${OUTPUT_SEGMENTATION_RGB} none custom $ITKSNAP_COLORMAP 0 6
+
+echo "Making segmentation mosaic."
+${ANTSPATH}/CreateTiledMosaic -i ${INPUT_T1} \
+                              -r ${OUTPUT_SEGMENTATION_RGB} \
+                              -o ${OUTPUT_SEGMENTATION_MOSAIC} \
+                              -a 0.3 \
+                              -t -1x-1 \
+                              -d 2 \
+                              -p mask \
+                              -s [3,mask,mask] \
+                              -x ${OUTPUT_SEGMENTATION_MASK}
